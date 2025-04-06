de1a667b85291465ce508664345ec52deeea4069
elsaferati <elsavferati@gmail.com>
2025-04-06 17:12:34 +0200

    update LoginPage.js

---
+++ src/views/LoginPage.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log("Logging in with", email, password);
+  };
+
+  return (
+    <div style={{ maxWidth: "400px", margin: "100px auto" }}>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        /><br /><br />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        /><br /><br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
