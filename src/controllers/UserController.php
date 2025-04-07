564cbfab71ec58a2e33ba154589cb072a577d395
elsaferati <elsavferati@gmail.com>
2025-04-07 22:22:49 +0200

    create userController

---
+++ src/controllers/UserController.php
@@ -0,0 +1,31 @@
+<?php
+require_once "UserModel.php";
+
+class UserController
+{
+    private $model;
+
+    public function __construct($model)
+    {
+        $this->model = $model;
+    }
+
+    public function register($data)
+    {
+        $name = $data["name"] ?? '';
+        $email = $data["email"] ?? '';
+        $password = $data["password"] ?? '';
+
+        if ($this->model->isEmailRegistered($email)) {
+            return ["error" => "Email already registered"];
+        }
+
+        $success = $this->model->registerUser($name, $email, $password);
+
+        if ($success) {
+            return ["success" => "User registered successfully"];
+        } else {
+            return ["error" => "Registration failed"];
+        }
+    }
+}
