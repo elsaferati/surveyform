7cecc041a427be101b2ddeaa9cc211d493839665
Vjona <lekajvjona@hotmail.com>
2025-04-29 22:00:53 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./views/LoginPage";
 import RegisterPage from "./views/RegisterPage";
 import UserTable from "./views/UserTable";
+import SurveyForm from "./views/SurveyForm";
+import ThankYou from "./views/Thankyou"; // Adjust path if needed
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/users" element={<UserTable />} />
+        <Route path="/survey" element={<SurveyForm />} />       {/* ✅ Add this */}
+        <Route path="/thank-you" element={<ThankYou />} />      {/* ✅ Add this */}
       </Routes>
     </Router>
   );
