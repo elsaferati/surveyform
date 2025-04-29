c9799a45d0456b6a23cad159c17ebdd919f7b57d
Vjona <lekajvjona@hotmail.com>
2025-04-30 00:14:51 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';   // ← this file must live in /src
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-
-
-
-
