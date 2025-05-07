eed1c4575fe7ec2a56e828d5fbdcf5452c24fd60
elsaferati <elsavferati@gmail.com>
2025-05-07 22:55:32 +0200

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-xl font-semibold">Survey Form</h1>
         <nav className="space-x-4">
-          <Link to="/" className="text-white hover:text-gray-300">Home</Link>
+          <Link to="/survey" className="text-white hover:text-gray-300">Home</Link>
           <Link to="/login" className="text-white hover:text-gray-300">Login</Link>
           <Link to="/register" className="text-white hover:text-gray-300">Register</Link>
         </nav>
